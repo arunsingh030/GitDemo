87a0947ed9d347ae98cde932eda3e394af9c2e47
Arun Kumar <ArunSingh030@gmail.com>
2024-02-22 16:46:59 -0500

    Another commit 446PM

--- src/test/java/stepDefinations/GitAddData.java
+++ src/test/java/stepDefinations/GitAddData.java
@@ -11,6 +11,18 @@ public class GitAddData {
 	{
 		System.out.println("This is GitDemo project Get Data method 02");
 	}
+	public void getData03()
+	{
+		System.out.println("This is GitDemo project Get Data method 03");
+	}
+	public void getData04()
+	{
+		System.out.println("This is GitDemo project Get Data method 04");
+	}
+	public void getData05()
+	{
+		System.out.println("This is GitDemo project Get Data method 05");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,7 +34,11 @@ public class GitAddData {
 		ad.getData02();
 		ad.getData01();
 		ad.getData02();
-		
+		ad.getData01();
+		ad.getData02();
+		ad.getData03();
+		ad.getData04();
+		ad.getData05();
 
 	}
 
